4039e09c77d8ce70977bb9306a3a3819efb19f41
aakash.n@care.ac.in <107519264+Aakash-a3an@users.noreply.github.com>
2024-02-22 19:08:02 +0530

    Updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
         <span className='text-3xl mb-2.5 flex font-semibold'>Hello <img src={hi} alt="Hi Emoji" className='object-cover h-8 w-8 ml-2 ' /> </span>
         <span className='lg:text-7xl text-4xl font-semibold font-Poppins justify-center '>I'm<span className='text-violet-400'> Aakash</span><br/>Web Designer</span>
         <p className='lg:text-lg text-lg lg:mt-4 mt-2 '>I'm a skilled web designer with experience in creating<br className='hidden'/> visually appealing and user friendly websites.</p>
-        <a href={resume} download='Aakash_Resume' ><button className='flex h-10 lg:text-lg text-sm items-center justify-center lg:mt-4 mt-2 pr-2 pb-1 bg-black hover:bg-violet-400 transition-all duration-200 hover:shadow-md dark:hover:shadow-xl font-semibold lg:w-40 w-32 rounded-full text-white'> <TbCloudDownload className='mx-2'/>Download CV</button></a>
+        <a href={resume} download='Aakash_Resume' ><button className='flex h-10 lg:text-lg text-sm items-center justify-center lg:mt-4 mt-2 pr-2 pb-1 bg-black  transition-all duration-200 hover:shadow-md dark:hover:shadow-xl font-semibold lg:w-40 w-32 rounded-full text-white'> <TbCloudDownload className='mx-2'/>Download CV</button></a>
       </div>
       <div className='hero lg:mr-8 lg:mt-0 mt-20'>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-scroll'
 const Projects = () => {
   return (
     <><div id='projects'>
-      <p className='text-[#d64aff] text-4xl font-bold font-Poppins flex justify-center items-center lg:mt-0 mt-[42rem]'>Projects</p>
+      <p className='text-[#9e5ed0] text-4xl font-bold font-Poppins flex justify-center items-center lg:mt-0 mt-[42rem]'>Projects</p>
       <div className='flex justify-center items-center'>
       <div className='flex flex-wrap max-w-[60rem] justify-center lg:mt-0  items-center'>
         <div className='h-[28rem] w-[20rem] flex flex-col rounded-2xl text-white hover:text-black bg-black hover:bg-white shadow-xl transition-all duration-300 m-8'>
